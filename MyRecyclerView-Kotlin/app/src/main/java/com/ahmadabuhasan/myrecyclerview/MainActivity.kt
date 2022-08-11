8a1c5a0f8aeb16060f4759c94dc9aec050892e82
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-11 20:50:52 +0700

    RecyclerView Mode List

--- MyRecyclerView-Kotlin/app/src/main/java/com/ahmadabuhasan/myrecyclerview/MainActivity.kt
+++ MyRecyclerView-Kotlin/app/src/main/java/com/ahmadabuhasan/myrecyclerview/MainActivity.kt
@@ -2,10 +2,30 @@ package com.ahmadabuhasan.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Mode List"
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }

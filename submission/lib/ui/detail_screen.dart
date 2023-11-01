bf61444fae64c69535312a9ca326a8b3820da28d
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2023-11-01 23:00:56 +0700

    Add StatelessWidget

---
+++ submission/lib/ui/detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:submission/model/data.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Data dataList;
+
+  const DetailScreen({Key? key, required this.dataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
